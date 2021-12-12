4382077557976051a66a337992d53ebc47954043
s1f102000051 <s1f102000051@iniad.org>
2021-12-12 10:37:05 +0900

    #8 - Creating The Room Join Page

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import CreateRoomView, GetRoom, RoomView
+from .views import CreateRoomView, GetRoom, JoinRoom, RoomView
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view()),
 ]
