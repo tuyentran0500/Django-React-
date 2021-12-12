ad674369f9199fb61a6b4052fbe65e7ca7155183
s1f102000051 <s1f102000051@iniad.org>
2021-12-12 11:19:09 +0900

    #9 - ComponentDidMount and Django Sessions

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import CreateRoomView, GetRoom, JoinRoom, RoomView
+from .views import CreateRoomView, GetRoom, JoinRoom, RoomView, UserInRoom
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
     path('join-room', JoinRoom.as_view()),
+    path('user-in-room', UserInRoom.as_view()),
 ]
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,10 +1,12 @@
 from django.db.models import query
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from rest_framework import generics, serializers, status
 from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 # Create your views here.
 
 class RoomView(generics.CreateAPIView):
@@ -66,5 +68,13 @@ class CreateRoomView(APIView):
                 self.request.session['room_code'] = room.code
                 return Response(RoomSerializer(room).data,  status =status.HTTP_201_CREATED)
         return Response({'Bad Request' : 'Invalid Data'}, status=status.HTTP_400_BAD_REQUEST)
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        data = {
+            'code':self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
     
 
